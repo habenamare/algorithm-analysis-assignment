269b652c6ad41f6310947c78aa03bbc55645eb25
Haben Amare <haben.amare@outlook.com>
2018-11-08 13:47:16 +0300

    :sparkles: add code for 'Arrays' assignment

---
+++ 01-arrays.py
@@ -0,0 +1,320 @@
+#! /usr/bin/python3
+import math
+
+# test arrays
+hollow_test_arrays = [
+   [1, 2, 0, 0, 0, 3, 4],
+   [1, 1, 1, 1, 0, 0, 0, 0, 0, 2, 1, 2, 18],
+   [1, 2, 0, 0, 3, 4],
+   [1, 2, 0, 0, 0, 3, 4, 5],
+   [3, 8, 3, 0, 0, 0, 3, 3],
+   [1, 4, 2, 0, 0, 0, 3, 4, 0],
+   [0, 1, 2, 0, 0, 0, 3, 4],
+   [0, 0, 0]
+]
+
+flip_flop_test_arrays = [
+   [],
+   [2],
+   [2, 2, 3, 4],
+   [1, 2, 3, 3, 4],
+   [2],
+   [0, 3, 4, -7],
+   [1, 2, 3, 4]
+]
+
+balanced_test_arrays = [
+   [2, 3, 6, 7],
+   [6, 3, 2, 7],
+   [6, 7, 2, 3, 12],
+   [6, 7, 2, 3, 14, 95],
+   [7, 15, 2, 3],
+   [16, 6, 2, 3],
+   [2],
+   [3],
+   []
+]
+
+odd_heavy_test_arrays = [
+   [1],
+   [2],
+   [1, 1, 1, 1, 1, 1],
+   [2, 4, 6, 8, 11],
+   [-2, -4, -6, -8, -11],
+   [16, 6, 37, 2, 3],
+   []
+]
+
+cumulative_test_arrays = [
+   [],
+   [1],
+   [0, 0, 0, 0, 0, 0],
+   [1, 1, 1, 1, 1, 1],
+   [3, 3, 6, 12, 24],
+   [-3, -3, -6, -12, -24],
+   [-3, -3, 6, 12, 24]
+]
+
+centered_test_arrays = [
+   [1, 2, 3, 4, 5],
+   [3, 2, 1, 4, 5],
+   [3, 2, 1, 4, 1],
+   [3, 2, 1, 1, 4, 6],
+   [1],
+   []
+]
+
+layered_test_arrays = [
+   [1, 1, 2, 2, 2, 3, 3],
+   [3, 3, 3, 3, 3, 3, 3],
+   [1, 2, 2, 2, 3, 3],
+   [2, 2, 2, 3, 3, 1, 1],
+   [2, 2, 2, 2],
+   [2, 2, 2],
+   [2, 1],
+   [2],
+   []
+]
+
+daphne_test_arrays = [
+   [1, -3, 5, -6],
+   [-3, 5, -6],
+   [1, -3, 5],
+   [-3, 5],
+   [-3],
+   [5],
+   [1, 1, -3, 5],
+   [-2, -2, -2],
+   [1, -1, 2, 2],
+   []
+]
+
+dual_test_arrays = [
+   [1, 2, 3, 0],
+   [1, 2, 2, 1, 3, 0],
+   [1, 1, 2, 2],
+   [1, 2, 1],
+   [1, 2],
+   []
+]
+
+
+# hollow array
+def is_hollow(a, length):
+   return "howllow function working"
+
+# flip-flop array
+def is_flip_flop(a, length):
+   if length <= 1:
+      return False
+
+   first_is_even = (a[0] % 2 == 0)
+   check_next_even = not first_is_even
+
+   for i in range(1, length):
+      if check_next_even and (a[i] % 2 != 0):
+         return False
+      elif (not check_next_even) and (a[i] % 2 == 0):
+         return False
+      
+      check_next_even = not check_next_even
+   
+   return True
+
+# balanced array
+def is_balanced(a, length):
+   if length == 0:
+      return True
+
+   first_is_even = (a[0] % 2 == 0)
+
+   if not first_is_even:
+      return False
+
+   check_next_is_even = False
+   for i in range(1, length):
+      if check_next_is_even and (a[i] % 2 != 0):
+         return False
+      elif (not check_next_is_even) and (a[i] % 2 == 0):
+         return False
+      
+      check_next_is_even = not check_next_is_even
+   
+   return True
+
+# odd-heavy array
+def is_odd_heavy(a, length):
+   if length == 0:
+      return False
+
+   first_is_even = (a[0] % 2 == 0)
+   if first_is_even:
+      # if the first element is even
+      return False
+   elif (not first_is_even) and length == 1:
+      # if the first element is odd and if it is the only element
+      return True
+
+   max_even = 0
+   min_odd = a[0]
+
+   for i in range(1, length):
+      i_is_even = (a[i] % 2 == 0);
+      if i_is_even and a[i] > max_even:
+         max_even = a[i]
+      elif (not i_is_even) and a[i] < min_odd:
+         min_odd = a[i]
+   
+   return min_odd > max_even
+
+# cumulative array
+def is_cumulative(a, length):
+   if length < 2:
+      return False
+
+   for i in range(1, length):
+      all_previous_elements_sum = 0
+
+      for j in range(0, i):
+         all_previous_elements_sum += a[j];
+   
+      if all_previous_elements_sum != a[i]:
+         return False
+   
+   return True
+
+# centered array
+def is_centered(a, length):
+   if length % 2 == 0:
+      # if number of elements is even (including 0), since there will be no middle element
+      return False
+   
+   if length == 1:
+      # satisfies the condition vacuously
+      return True
+   
+   middle_element_index = math.floor(length / 2)
+   middle_element = a[middle_element_index]
+   
+   # find the minimum element excluding the middle element
+   min_element = a[0];
+   for i in range(1, length):
+      if i == middle_element_index:
+         continue
+
+      if a[i] < min_element:
+         min_element = a[i]
+   
+   # note: strictly less than
+   return middle_element < min_element
+   
+# layered array
+def is_layered(a, length):
+   if length < 4:
+      # since each element should appear two or more times
+      return False
+   
+   current_element = a[0]
+   current_element_count = 1
+
+   for i in range(1, length):
+      if current_element_count < 2 and a[i] != current_element:
+         return False
+      elif a[i] == current_element:
+         current_element_count += 1
+      else:
+         if a[i] < current_element:
+            # since not in ascending order
+            return False
+         else:
+            current_element = a[i]
+            current_element_count = 1
+   
+   # check if the last element appeared two or more times
+   # (if the code reached this part without returning)
+   return current_element_count >= 2
+
+# daphne array
+def is_daphne(a, length):
+   if length == 0:
+      return False
+   elif length == 1:
+      return True
+   
+   first_is_positive = a[0] > 0;
+   check_next_positive = not first_is_positive
+
+   for i in range(1, length):
+      if check_next_positive and a[i] < 0:
+         return False
+      elif not check_next_positive and a[i] > 0:
+         return False
+      
+      check_next_positive = not check_next_positive
+
+   return True
+
+# dual array
+def is_dual(a, length):
+   if length % 2 != 0:
+      # since array should have even number of elements
+      return False
+
+   if length == 0:
+      return True
+   
+   first_pair_sum = a[0] + a[1]
+
+   for i in range(2, length, 2):
+      current_pair_sum = a[i] + a[i+1]
+      if current_pair_sum != first_pair_sum:
+         return False
+   
+   return True
+
+testing_types = [
+   "hollow",
+   "flip-flop",
+   "balanced",
+   "odd-heavy",
+   "cumulative",
+   "centered",
+   "layered",
+   "daphne",
+   "dual"
+]
+
+## testing
+def test(test_for_type):
+   if test_for_type == "hollow":
+      for test_array in hollow_test_arrays:
+         print(is_hollow(test_array, len(test_array)))
+   elif test_for_type == "flip-flop":
+      for test_array in flip_flop_test_arrays:
+         print(is_flip_flop(test_array, len(test_array)))
+   elif test_for_type == "balanced":
+      for test_array in balanced_test_arrays:
+         print(is_balanced(test_array, len(test_array))) 
+   elif test_for_type == "odd-heavy":
+      for test_array in odd_heavy_test_arrays:
+         print(is_odd_heavy(test_array, len(test_array)))        
+   elif test_for_type == "cumulative":
+      for test_array in cumulative_test_arrays:
+         print(is_cumulative(test_array, len(test_array)))
+   elif test_for_type == "centered":
+      for test_array in centered_test_arrays:
+         print(is_centered(test_array, len(test_array)))
+   elif test_for_type == "layered":
+      for test_array in layered_test_arrays:
+         print(is_layered(test_array, len(test_array)))
+   elif test_for_type == "daphne":
+      for test_array in daphne_test_arrays:
+         print(is_daphne(test_array, len(test_array)))
+   elif test_for_type == "dual":
+      for test_array in dual_test_arrays:
+         print(is_dual(test_array, len(test_array)))
+   else:
+      print("incorrect testing type")
+
+## run test for the last testing type
+test(testing_types[-1]);
